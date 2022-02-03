e26449a00cf9b29eaa66e48b94dc023d4ad24a13
Leonid <lkurgan55@gmail.com>
2022-02-03 16:08:46 +0200

    user menu

--- ZPZ/lab_1.py
+++ ZPZ/lab_1.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from hashlib import md5
+from turtle import clear
 
 USERS = {'admin': ''}
 
@@ -18,10 +19,65 @@ else:
 
 class User():
     def __init__(self, login):
-        pass
+        self.login = login
 
-    def change_pass(self):
-        pass
+    def menu(self, root):
+        root.title('Menu')
+        tk.Label(root, text=f'You are "{self.login}"').pack()
+        
+        old_pass = tk.StringVar()
+        tk.Label(root, text='Change my password').pack(pady=(20,0))
+        old_pass_label = ttk.Label(root, text="old password:")
+        old_pass_label.pack(pady=10)
+        old_pass_entry = ttk.Entry(root, width=25, textvariable=old_pass, show="*")
+        old_pass_entry.pack()
+        old_pass_entry.focus()
+        
+        password1 = tk.StringVar()
+        password1_label = ttk.Label(root, text="new password:")
+        password1_label.pack(pady=10)
+        password1_entry = ttk.Entry(root, width=25, textvariable=password1, show="*")
+        password1_entry.pack()
+
+        password2 = tk.StringVar()
+        password2_label = ttk.Label(root, text="repeat new password:")
+        password2_label.pack(pady=10)
+        password2_entry = ttk.Entry(root, width=25, textvariable=password2, show="*")
+        password2_entry.pack()
+
+        login_button = ttk.Button(root, text="change", command=lambda: change_pass(self.login, old_pass, password1, password2))
+        login_button.pack(pady=10) 
+
+        login_button = ttk.Button(root, text="Exit", command=lambda: exit(root))
+        login_button.pack(pady=10)        
+
+def exit(root):
+    clear_window(root)
+    login_window(root)
+
+
+def change_pass(login, old_pass, password1, password2):
+    old_pass = md5(old_pass.get().encode()).hexdigest()
+    password1 = md5(password1.get().encode()).hexdigest()
+    password2 = md5(password2.get().encode()).hexdigest()
+    if (login in USERS.keys()):
+            if (USERS[login] == old_pass or USERS[login] == ""):
+                if password1 == password2:
+                    USERS[login] = password1
+                    with open('database.json', 'w') as fp:
+                        json.dump(USERS, fp)
+                    messagebox.showinfo("Good", f"You have changed password for '{login}'")
+                else:
+                    messagebox.showerror("Error", "Passwords are different")
+            else:
+                messagebox.showerror("Error", "Wrong old password")
+    else:
+        messagebox.showerror("Error", "Login doesn`t exist")
+    print(login, password1)
+
+def clear_window(root):
+    for widget in root.winfo_children():
+        widget.destroy()
 
 def check_user(login, password):
     login = login.get()
@@ -29,11 +85,16 @@ def check_user(login, password):
     if (login in USERS.keys()):
             if (USERS[login] == password or USERS[login] == ""):
                 messagebox.showinfo("Good", f"You have signed in by'{login}'")
+                clear_window(root)
+                USER = User(login)
+                USER.menu(root)
+
             else:
                 messagebox.showerror("Error", "Wrong password")
     else:
         messagebox.showerror("Error", "Login doesn`t exist")
     print(login, password)
+    
 
 def register_user(login2, password1, password2):
     login = login2.get()
@@ -73,8 +134,6 @@ def login_window(root):
     password_entry = ttk.Entry(root, width=25, textvariable=password, show="*")
     password_entry.pack()
 
-    
-
     login_button = ttk.Button(root, text="Login", command=lambda: check_user(login, password))
     login_button.pack(pady=10)
 
@@ -106,7 +165,6 @@ def login_window(root):
 
 
 root = tk.Tk()
-
 login_window(root)
 
 root.mainloop()
